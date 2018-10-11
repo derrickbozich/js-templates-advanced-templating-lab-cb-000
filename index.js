758494bf840702c387154be5cf6b9584d9fc7642
derrick b <dbozich1@gmail.com>
2018-10-11 16:37:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function handlebarsSetup(){
   Handlebars.registerHelper("displayIngredient", function(ingredient){
     return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
   });
-  Handlebars.registerPartial("recipeDetailsPartial", document.getElementById('recipe-details-partial').innerHTML);
-  Handlebars.registerPartial("recipeFormPartial", document.getElementById('recipe-form-partial').innerHTML);
+  // Handlebars.registerPartial("recipeDetailsPartial", document.getElementById('recipe-details-partial').innerHTML);
+  // Handlebars.registerPartial("recipeFormPartial", document.getElementById('recipe-form-partial').innerHTML);
 
 }
 
