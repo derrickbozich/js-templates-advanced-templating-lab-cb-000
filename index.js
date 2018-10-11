7da9af0be4597d6d8982aadc92903423849a640e
derrick b <dbozich1@gmail.com>
2018-10-11 16:31:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
+function handlebarsSetup(){
+  //handlebars registrations are here
+  Handlebars.registerHelper("displayIngredient", function(ingredient){
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  });
+  Handlebars.registerPartial("recipeDetailsPartial", document.getElementById('recipe-details-partial').innerHTML);
+  Handlebars.registerPartial("recipeFormPartial", document.getElementById('recipe-form-partial').innerHTML);
+
+}
+
 function init() {
   //put any page initialization/handlebars initialization here
-  Handlebars.registerHelper('displayIngredient', function() {
-    return new Handlebars.SafeString(this.name)
-  });
-
-
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipeDetailsPartial").innerHTML)
-  function renderDetailsPartial() {
-    let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
-    let html = template({description: this.description, ingredients: this.ingredients});
-    return html;
-  }
-
-  // Handlebars.registerPartial('recipeFormPartial', function() {
-  //
-  // });
-
 
 }
 document.addEventListener("DOMContentLoaded", function(event) {
