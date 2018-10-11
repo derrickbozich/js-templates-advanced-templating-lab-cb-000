5de04cc38139a4474cf7376bafcdd9697cacb422
derrick b <dbozich1@gmail.com>
2018-10-11 15:34:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerHelper('displayIngredient', function() {
+    return new Handlebars.SafeString(this.name)
+  });
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
