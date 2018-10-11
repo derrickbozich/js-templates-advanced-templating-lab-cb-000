19bdef887df98d666201ae6972e75e197e09ee9c
derrick b <dbozich1@gmail.com>
2018-10-11 16:13:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ function init() {
   Handlebars.registerHelper('displayIngredient', function() {
     return new Handlebars.SafeString(this.name)
   });
+
+
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipeDetailsPartial").innerHTML)
+  function renderDetailsPartial() {
+    let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+    let html = template({description: this.decription, ingredients: this.ingredients});
+  }
+
+  Handlebars.registerPartial('recipeFormPartial', function() {
+
+  });
+
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
