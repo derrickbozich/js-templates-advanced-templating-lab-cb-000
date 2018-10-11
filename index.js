568c79f97a1beb0e7729969c14b8901a3ba9a971
derrick b <dbozich1@gmail.com>
2018-10-11 17:46:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ function initForm(){
 }
 
 //renders a new recipe from the values inputted into initial form
-// function createRecipe(){
-//   let recipe = getRecipeValues();
-//   let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
-//   let html = template(recipe)
-//
-//   document.getElementById('main').innerHTML = html;
-// }
+function createRecipe(){
+  let recipe = getRecipeValues();
+  let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  let html = template(recipe)
+
+  document.getElementById('main').innerHTML = html;
+}
 
 
 
 //renders an edited recipe
-// function updateRecipe(){
-//   let recipe = getRecipeValues();
-//   let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
-//   let html = template(recipe)
-//
-//   document.getElementById('main').innerHTML = html;
-// }
+function updateRecipe(){
+  let recipe = getRecipeValues();
+  let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  let html = template(recipe)
+
+  document.getElementById('main').innerHTML = html;
+}
 
 //retrieves recipe values from the form that is currently rendered
 function getRecipeValues(){
@@ -56,7 +56,7 @@ function handlebarsSetup(){
 function init() {
   //put any page initialization/handlebars initialization here
   handlebarsSetup();
-  initForm();
+  // initForm();
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
